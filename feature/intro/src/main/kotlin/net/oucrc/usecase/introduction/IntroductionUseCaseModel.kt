feb9d77dda89300345736211b711182ccca099b0
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-16 16:12:08 +0900

    [add] IntroductionUseCaseModelを追加

---
+++ feature/intro/src/main/kotlin/net/oucrc/usecase/introduction/IntroductionUseCaseModel.kt
@@ -0,0 +1,22 @@
+package net.oucrc.usecase.introduction
+
+import kotlinx.datetime.Instant
+import net.oucrc.domain.introduction.Introduction
+import net.oucrc.domain.introduction.IntroductionContent
+import net.oucrc.domain.introduction.IntroductionId
+
+data class IntroductionUseCaseModel(
+    val id: IntroductionId,
+    val content: IntroductionContent,
+    val createdAt: Instant,
+    val modifiedAt: Instant,
+) {
+    companion object {
+        fun from(introduction: Introduction) = IntroductionUseCaseModel(
+            id = introduction.id,
+            content = introduction.content,
+            createdAt = introduction.createdAt,
+            modifiedAt = introduction.modifiedAt,
+        )
+    }
+}
