aea3f580cf624859e4dc656790d301db02f285f7
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-11-16 16:33:21 +0900

    [add] UpsertIntroductionUseCaseを追加

--- feature/intro/src/main/kotlin/net/oucrc/domain/introduction/Introduction.kt
+++ feature/intro/src/main/kotlin/net/oucrc/domain/introduction/Introduction.kt
@@ -1,12 +1,28 @@
 package net.oucrc.domain.introduction
 
+import java.util.UUID
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
-class Introduction(
+class Introduction private constructor(
     val id: IntroductionId,
     val content: IntroductionContent,
     val modifiedAt: Instant,
-)
+) {
+    fun update(content: IntroductionContent) = Introduction(
+        id = id,
+        content = content,
+        modifiedAt = Clock.System.now(),
+    )
+
+    companion object {
+        fun create(content: IntroductionContent) = Introduction(
+            id = IntroductionId(UUID.randomUUID().toString()),
+            content = content,
+            modifiedAt = Clock.System.now(),
+        )
+    }
+}
 
 @JvmInline
 value class IntroductionId(val value: String)
